cfbefdf8e5759212923221810bc39a9ae7689241
eunicepaulson <eunicegp159@gmail.com>
2020-09-16 11:39:55 +0000

    More printing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+
 class HelloWorld{
 public static void main(String [] args){
+System.out.println("Hello World");
+System.out.println("Hello World");
 System.out.println("Hello World");
 System.out.println("The sum of 3 and 5 is "+(3+5));
 }
